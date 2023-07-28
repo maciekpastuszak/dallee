35c3f001d70dfb308e984d92b93e11214c507556
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-07-28 10:27:11 +0200

    add app wrapper

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,16 @@ import React from 'react'
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 
 import { logo } from './assets';
+import { Home, CreatePost } from './pages';
 
 const App = () => {
   return (
     <div className="text-3xl font-bold underline">
-      Hello world
+      <BrowserRouter>
+        <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
+
+        </header>
+      </BrowserRouter>
     </div>
   )
 }
