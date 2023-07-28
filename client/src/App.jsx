e4cddc1b9026a3b76f1752fa21e14dd4840c10a3
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-07-28 10:34:26 +0200

    add main wrapper

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,6 +17,9 @@ const App = () => {
             Create
           </Link>
         </header>
+        <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
+
+        </main>
       </BrowserRouter>
     </div>
   )
