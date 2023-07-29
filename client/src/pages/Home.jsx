b1fca0e5187612963d6f260230a9900efdbc9198
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-07-29 23:00:31 +0200

    add imports and basic hooks to Home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+
+import { Card, FormField, Loader } from '../components';
 
 const Home = () => {
+  const [loading, setLoading] = useState(false);
+  const [allPosts, setAllPosts] = useState(null);
   return (
     <div>Home</div>
   )
