8b81ed6cba63068950266c9c3d8e643e3fb0228c
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-07-30 23:43:58 +0200

    add state for searchText

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -6,6 +6,7 @@ const Home = () => {
   const [loading, setLoading] = useState(false);
   const [allPosts, setAllPosts] = useState(null);
 
+  const [searchText, setSearchText] = useState('');
   return (
     <section className='max-w-7xl mx-auto'>
       <div>
@@ -22,7 +23,15 @@ const Home = () => {
            <div className="flex justify-center items-center">
               <Loader />
           </div>
-        ) : () }
+        ) : (
+          <>
+            {searchText && (
+              <h2 className='font-medium text-[#666e75] text-xl mb-3'>
+                Showing results for <span className='text-[#222328]'>{searchText}</span>
+              </h2>
+            )}
+          </>
+        ) }
       </div>
     </section>
   )
