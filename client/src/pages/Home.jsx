98728526e1ae32735fd958341108bb25293b6e33
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-08-01 00:03:18 +0200

    update data in RenderCards to fix error

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -44,12 +44,12 @@ const Home = () => {
             <div className='grid lg:grid-cols-4 sm:grid-col-3 xs:grid-cols-2 grid-cols-1 gap-3'>
               {searchText ? (
                 <RenderCards 
-                  data="searchResults"
+                  data={[]}
                   title="No search results found"
                 />
                ) : (
                   <RenderCards 
-                    data="allPosts"
+                    data={[]}
                     title="No posts found"
                   />
               )}
