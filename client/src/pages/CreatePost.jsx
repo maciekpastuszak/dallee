6eaf79149497fc3d22e67e85d2cdd394a1931637
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-08-03 17:06:07 +0200

    add handleSurpriseMe

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -21,7 +21,10 @@ const CreatePost = () => {
 
   const handleChange = (e) => setForm({ ...form, [e.target.name]: e.target.value });
 
-  const handleSurpriseMe = () => {}
+  const handleSurpriseMe = () => {
+    const randomPrompt = getRandomPrompt(form.prompt);
+    setForm({ ...form, prompt: randomPrompt });
+  };
 
   return (
     <section className='max-w-7xl mx-auto'>
