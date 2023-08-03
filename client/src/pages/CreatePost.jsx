830067b503ee31660125955ffdc7fa569d3e6e07
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-08-03 16:58:31 +0200

    add message to share the img

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -83,6 +83,10 @@ const CreatePost = () => {
                 {generatingImg ? 'Generating...': 'Generate'}
               </button>
         </div>
+
+        <div className="mt-10">
+              <p className='mt-2 text-[#666e75] text-[14px]'>Once you  have created the image you want, you can share it with others in the community</p>
+        </div>
       </form>
 
     </section>
