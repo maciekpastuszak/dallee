6f87a921a3eea2f955e5ddc218a2c721eaf671be
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-08-15 11:32:52 +0200

    add missing catch err and export in connect to db

--- server/mongodb/connect.js
+++ server/mongodb/connect.js
@@ -5,4 +5,7 @@ const connectDB = (url) => {
 
     mongoose.connect(url)
         .then(() => console.log('MongoDB connected'))
+        .catch((err) => console.log(err));
 }
+
+export default connectDB;
