dcfb20b8b1b15251ad151d0a230a5e8ba3c8d74a
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-08-16 19:26:59 +0200

    update useEffect

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -22,18 +22,28 @@ const Home = () => {
   useEffect(() => {
     const fetchPosts = async () => {
       setLoading(true);
-
       try {
-        
+        const response = await fetch('http://localhost:8080/api/v1/post',{
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        })
+
+        if(response.ok) {
+          const result = await response.json();
+
+          setAllPosts(result.data.reverse());
+        }
       } catch (error) {
-        
+        alert(error);
       } finally {
         setLoading(false)
       }
     }
   
     fetchPosts();
-  }, [third])
+  }, [])
   
 
   return (
@@ -67,7 +77,7 @@ const Home = () => {
                 />
                ) : (
                   <RenderCards 
-                    data={[]}
+                    data={allPosts}
                     title="No posts found"
                   />
               )}
