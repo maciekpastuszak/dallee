c295371f6f993793c76af06b21db993b1ae5b849
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-08-16 16:18:51 +0200

    create new post in db

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -14,4 +14,27 @@ cloudinary.config({
     api_secret: process.env.CLOUDINARY_API_SECRET,
 })
 
+//GET ALL POSTS
+router.route('/').get(async(req,res) => {
+   try {
+    const { name, prompt, photo } = req.body;
+    const photoUrl = await cloudinary.uploader.upload(photo);
+
+    const newPost = await Post.create({
+        name,
+        prompt,
+        photo: photoUrl.url,
+    })
+
+    res.status(201).json({ success: true, data: newPost });
+   } catch (error) {
+        res.status(500).json({success: false, message: error})
+   }
+});
+
+//CREATE A POST
+router.route('/').post(async(req,res) => {
+
+});
+
 export default router;
