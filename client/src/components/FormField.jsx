a08f29e0aa8e55a3c718f91cf812fae51bb36f3b
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-08-16 16:01:57 +0200

    bugfix - typos in FormField

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
 
-const FormField = ({ labelName, type, name, placeholder, value, handleChange, isSurpriseMe, handleSurtpriseMe }) => {
+const FormField = ({ labelName, type, name, placeholder, value, handleChange, isSurpriseMe, handleSurpriseMe }) => {
   return (
     <div>
       <div className="flex items-center gap-2 mb-2">
         <label 
           htmlFor={name}
-          className="block text-sm font-medium text-grey-900"
+          className="block text-sm font-medium text-gray-900"
         >
           {labelName}
         </label>
         {isSurpriseMe && (
           <button
             type="button"
-            onClick={handleSurtpriseMe}
-            className="font-semibold text-xs bg-[#ECECF1] py-1 px-2 rounded-[5px] text black"
+            onClick={handleSurpriseMe}
+            className="font-semibold text-xs bg-[#EcECF1] py-1 px-2 rounded-[5px] text black"
           >
             Surprise me
           </button>
